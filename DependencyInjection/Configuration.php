0c203449ac6db9bebef88eaa959c67acba346926
PandiX <pandix211@gmail.com>
2018-10-09 15:30:10 +0200

    Instanciate bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tms\Bundle\ThemeBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('tms_theme');
+
+
+        return $treeBuilder;
+    }
+}
---
+++ TmsThemeBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Tms\Bundle\ThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TmsThemeBundle extends Bundle
+{
+}
